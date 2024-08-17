f65538b5cf4948b035860efbcfa95af17505c0bd
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-08-17 22:16:44 +0330

    codes about firstname, lastname and email were wwritten

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 //variables
 const radioElements = document.querySelectorAll(".query-radios input");
+const submitButton = document.getElementById("submit-button");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const firstNameError = document.getElementById("firstName-error");
+const lastNameError = document.getElementById("lastName-error");
+const email = document.getElementById("email");
+const emailError = document.getElementById("email-error");
 
 console.dir(radioElements);
 
@@ -18,13 +25,53 @@ const rdioButtonHandler = (e) => {
 const radioMouseOverHandler = (e) => {
   console.dir(e.target);
 
-//   if (e.target.checked) {
-//     e.target.style.accentColor = "#02745b";
-//   }
+  //   if (e.target.checked) {
+  //     e.target.style.accentColor = "#02745b";
+  //   }
 };
 
+const clearStyles = () => {
+  firstName.classList.remove("error");
+  lastName.classList.remove("error");
+  firstNameError.style.display = "none";
+  lastNameError.style.display = "none";
+
+
+  email.classList.remove("error");
+  emailError.style.display = "none";
+};
+
+const validateForm = () => {
+  clearStyles();
+
+  if (!firstName.value) {
+    firstName.classList.add("error");
+    firstNameError.style.display = "block";
+  }
+
+  if (!lastName.value) {
+    lastName.classList.add("error");
+    lastNameError.style.display = "block";
+  }
+
+  if (!email.value) {
+    email.classList.add("error");
+    emailError.style.display = "block";
+  }
+  return false;
+};
+
+const submitHandler = (e) => {
+  if (!validateForm()) {
+    console.log("success...");
+  } else {
+    console.log("error!!!");
+  }
+};
 //Events
 radioElements.forEach((radio) => {
   radio.addEventListener("click", rdioButtonHandler);
-//   radio.addEventListener("mouseover", radioMouseOverHandler);
+  //   radio.addEventListener("mouseover", radioMouseOverHandler);
 });
+
+submitButton.addEventListener("click", submitHandler);
