e0df6c1dad38045faadc26397a20adc311992486
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-08-18 08:58:37 +0330

    add validate for fields in page

--- js/script.js
+++ js/script.js
@@ -7,8 +7,12 @@ const firstNameError = document.getElementById("firstName-error");
 const lastNameError = document.getElementById("lastName-error");
 const email = document.getElementById("email");
 const emailError = document.getElementById("email-error");
-
-console.dir(radioElements);
+const queryTypeRadios = document.getElementsByName("queryType");
+const queryTypeError = document.getElementById("queryType-error");
+const message = document.getElementById("message");
+const messageError = document.getElementById("message-error");
+const contactedCheckbox = document.getElementById("contacted");
+const contactedkError = document.getElementById("contacted-error");
 
 //Handlers
 const rdioButtonHandler = (e) => {
@@ -36,11 +40,24 @@ const clearStyles = () => {
   firstNameError.style.display = "none";
   lastNameError.style.display = "none";
 
-
   email.classList.remove("error");
   emailError.style.display = "none";
+
+  queryTypeError.style.display = "none";
+
+  message.classList.remove("error");
+  messageError.style.display = "none";
+
+  contactedkError.style.display = "none";
 };
 
+const validateRadios = (element) => {
+  for (let i = 0; i < element.length; i++) {
+    if (element[i].checked) return true;
+  }
+
+  return false;
+};
 const validateForm = () => {
   clearStyles();
 
@@ -58,6 +75,20 @@ const validateForm = () => {
     email.classList.add("error");
     emailError.style.display = "block";
   }
+
+  if (!validateRadios(queryTypeRadios)) {
+    // email.classList.add("error");
+    queryTypeError.style.display = "block";
+  }
+
+  if (!message.value) {
+    message.classList.add("error");
+    messageError.style.display = "block";
+  }
+
+  if (!contactedCheckbox.checked){
+    contactedkError.style.display = "block";
+  }
   return false;
 };
 
@@ -68,6 +99,7 @@ const submitHandler = (e) => {
     console.log("error!!!");
   }
 };
+
 //Events
 radioElements.forEach((radio) => {
   radio.addEventListener("click", rdioButtonHandler);
@@ -75,3 +107,8 @@ radioElements.forEach((radio) => {
 });
 
 submitButton.addEventListener("click", submitHandler);
+
+// pasteCatcher.focus();
+firstName.addEventListener("focus", function (e) {
+  e.preventDefault();
+});
